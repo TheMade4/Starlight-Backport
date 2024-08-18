de2fe5c71c64035cf199c6a9f26d292fcec20a55
TheMade4 <91578379+TheMade4@users.noreply.github.com>
2024-08-18 18:14:40 +0300

    Backport starlight 1.20.2 bug fixes

--- src/main/java/ca/spottedleaf/starlight/mixin/common/chunk/ChunkAccessMixin.java
+++ src/main/java/ca/spottedleaf/starlight/mixin/common/chunk/ChunkAccessMixin.java
@@ -2,12 +2,30 @@ package ca.spottedleaf.starlight.mixin.common.chunk;
 
 import ca.spottedleaf.starlight.common.chunk.ExtendedChunk;
 import ca.spottedleaf.starlight.common.light.SWMRNibbleArray;
+import ca.spottedleaf.starlight.common.light.StarLightEngine;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ImposterProtoChunk;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+import net.minecraft.world.level.chunk.UpgradeData;
+import net.minecraft.world.level.levelgen.blending.BlendingData;
+import net.minecraft.world.level.lighting.ChunkSkyLightSources;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChunkAccess.class)
 public abstract class ChunkAccessMixin implements ExtendedChunk {
+
+    @Shadow
+    protected ChunkSkyLightSources skyLightSources;
+
 
     @Unique
     private volatile SWMRNibbleArray[] blockNibbles;
@@ -60,4 +78,37 @@ public abstract class ChunkAccessMixin implements ExtendedChunk {
     public void setBlockEmptinessMap(final boolean[] emptinessMap) {
         this.blockEmptinessMap = emptinessMap;
     }
+
+    /**
+     * @reason Remove unused skylight sources, and initialise nibble arrays.
+     * @author Spottedleaf
+     */
+    @Inject(
+            method = "<init>",
+            at = @At(
+                    value = "RETURN"
+            )
+    )
+    private void nullSources(ChunkPos chunkPos, UpgradeData upgradeData, LevelHeightAccessor levelHeightAccessor,
+                             Registry registry, long l, LevelChunkSection[] levelChunkSections, BlendingData blendingData,
+                             CallbackInfo ci) {
+        this.skyLightSources = null;
+        if (!((Object)this instanceof ImposterProtoChunk)) {
+            this.setBlockNibbles(StarLightEngine.getFilledEmptyLight(levelHeightAccessor));
+            this.setSkyNibbles(StarLightEngine.getFilledEmptyLight(levelHeightAccessor));
+        }
+    }
+
+    /**
+     * @reason Remove unused skylight sources
+     * @author Spottedleaf
+     */
+    @Redirect(
+            method = "initializeLightSources",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/lighting/ChunkSkyLightSources;fillFrom(Lnet/minecraft/world/level/chunk/ChunkAccess;)V"
+            )
+    )
+    private void skipInit(final ChunkSkyLightSources instance, final ChunkAccess chunkAccess) {}
 }
--- src/main/java/ca/spottedleaf/starlight/mixin/common/chunk/ProtoChunkMixin.java
+++ src/main/java/ca/spottedleaf/starlight/mixin/common/chunk/ProtoChunkMixin.java
@@ -1,37 +1,29 @@
 package ca.spottedleaf.starlight.mixin.common.chunk;
 
-import ca.spottedleaf.starlight.common.light.StarLightEngine;
 import ca.spottedleaf.starlight.common.chunk.ExtendedChunk;
-import net.minecraft.core.Registry;
-import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.LevelHeightAccessor;
-import net.minecraft.world.level.chunk.ImposterProtoChunk;
-import net.minecraft.world.level.chunk.LevelChunkSection;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.chunk.ProtoChunk;
-import net.minecraft.world.level.chunk.UpgradeData;
-import net.minecraft.world.level.levelgen.blending.BlendingData;
-import net.minecraft.world.ticks.ProtoChunkTicks;
+import net.minecraft.world.level.lighting.ChunkSkyLightSources;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ProtoChunk.class)
 public abstract class ProtoChunkMixin implements ExtendedChunk {
 
     /**
-     * Initialises the nibble arrays to default values.
-     * TODO since this is a constructor inject, check for new constructors on update.
+     * @reason Remove unused skylight sources
+     * @author Spottedleaf
      */
-    @Inject(
-            method = "<init>(Lnet/minecraft/world/level/ChunkPos;Lnet/minecraft/world/level/chunk/UpgradeData;[Lnet/minecraft/world/level/chunk/LevelChunkSection;Lnet/minecraft/world/ticks/ProtoChunkTicks;Lnet/minecraft/world/ticks/ProtoChunkTicks;Lnet/minecraft/world/level/LevelHeightAccessor;Lnet/minecraft/core/Registry;Lnet/minecraft/world/level/levelgen/blending/BlendingData;)V",
-            at = @At("TAIL")
+    @Redirect(
+            method = "setBlockState",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/lighting/ChunkSkyLightSources;update(Lnet/minecraft/world/level/BlockGetter;III)Z"
+            )
     )
-    public void onConstruct(ChunkPos chunkPos, UpgradeData upgradeData, LevelChunkSection[] levelChunkSections, ProtoChunkTicks protoChunkTicks, ProtoChunkTicks protoChunkTicks2, LevelHeightAccessor levelHeightAccessor, Registry registry, BlendingData blendingData, CallbackInfo ci) {
-        if ((Object)this instanceof ImposterProtoChunk) {
-            return;
-        }
-        this.setBlockNibbles(StarLightEngine.getFilledEmptyLight(levelHeightAccessor));
-        this.setSkyNibbles(StarLightEngine.getFilledEmptyLight(levelHeightAccessor));
+    private boolean skipLightSources(final ChunkSkyLightSources instance, final BlockGetter blockGetter,
+                                     final int x, final int y, final int z) {
+        return false;
     }
 }
